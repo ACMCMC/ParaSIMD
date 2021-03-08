3569d9419367d926280a5847428f45baabbf906e
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2021-03-08 12:15:41 +0100

    Cambio del script

--- script_medicion.sh
+++ script_medicion.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 make # Compilamos
 rm medidas.csv # Limpiamos el fichero de resultados
+touch medidas.csv
+echo 'ID_PRUEBA,C,F,T_TOTAL,T_POR_ACCESO' > medidas.csv
 lista_c="4 8 20 40"
 lista_l="384 1152 4096 12288 49152 73728 196608 393216"
 #lista_c="4 8"
 #lista_f="20 40"
 for id_prueba in {1..100};
 do
+    echo "=========== ID DE PRUEBA: $id_prueba ==========="
     for c in $lista_c;
     do
         for l in $lista_l;
