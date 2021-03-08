ea62b70f7edd2ccb6a0e3240a360a2790f5ad0c4
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2021-03-08 13:09:15 +0100

    Cambiado el script

--- script_medicion.sh
+++ script_medicion.sh
@@ -3,11 +3,11 @@ make # Compilamos
 rm medidas.csv # Limpiamos el fichero de resultados
 touch medidas.csv
 echo 'ID_PRUEBA,C,F,T_TOTAL,T_POR_ACCESO' > medidas.csv
-lista_c="4 8 20 40"
+lista_c="4 8 16 20 40 60 100 128"
 lista_l="384 1152 4096 12288 49152 73728 196608 393216"
 #lista_c="4 8"
 #lista_f="20 40"
-for id_prueba in {1..100};
+for id_prueba in {1..50};
 do
     echo "=========== ID DE PRUEBA: $id_prueba ==========="
     for c in $lista_c;
