287c377c5100b9923ad782abb7b987280cb8a8a6
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2021-03-21 13:47:45 +0100

    Scripts

--- script_medicion_entregar.sh
+++ script_medicion_entregar.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 make # Compilamos
 rm medidas_red1.csv # Limpiamos el fichero de resultados
-touch medidas_red1.csv
-echo 'ID_PRUEBA,C,F,L,T_TOTAL,T_POR_ACCESO' > medidas_red1.csv
+echo 'ID_PRUEBA,C,F,L,T_TOTAL,T_POR_ACCESO' > medidas_red1.csv # Cabecera del fichero
 rm medidas_red2.csv # Limpiamos el fichero de resultados
-touch medidas_red2.csv
-echo 'ID_PRUEBA,C,F,L,T_TOTAL,T_POR_ACCESO' > medidas_red2.csv
-lista_c="4 8 20 40"
-lista_l="384 1152 4096 12288 49152 73728 196608 393216"
-for id_prueba in {1..50};
+echo 'ID_PRUEBA,C,F,L,T_TOTAL,T_POR_ACCESO' > medidas_red2.csv # Cabecera del fichero
+lista_c="4 8 20 40" # Valores de C para probar
+lista_l="384 1152 4096 12288 49152 73728 196608 393216" # Valores de L para probar
+for id_prueba in {1..50}; # Repetimos las pruebas 50 veces
 do
     echo "=========== ID DE PRUEBA: $id_prueba ==========="
-    for c in $lista_c;
+    for c in $lista_c; # Para cada valor de C...
     do
-        for l in $lista_l;
+        for l in $lista_l; # Recorremos todos los valores de L...
         do
-        f=$(bc <<< "scale=2; (64/(8*$c))*$l")
-        f=$(echo "$f" | awk '{printf("%.0f\n", $1)}')
-            echo "Ejecutando prueba con C=$c L=$l F=$f"
-            eval "./codigo_1.out $c $f $l $id_prueba"
-            eval "./codigo_2.out $c $f $l $id_prueba"
+        f=$(bc <<< "scale=2; (64/(8*$c))*$l") # Calculamos el numero de filas que se corresponden con ese valor de L
+        f=$(echo "$f" | awk '{printf("%.0f\n", $1)}') # Eliminamos los decimales
+            echo "Ejecutando prueba con C=$c L=$l F=$f" # Información por pantalla
+            eval "./codigo_1.out $c $f $l $id_prueba" # Ejecutamos el primer experimento
+            eval "./codigo_2.out $c $f $l $id_prueba" # Ejecutamos el segundo experimento
         done
     done
 done
